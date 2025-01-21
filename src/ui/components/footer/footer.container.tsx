d699ea6d19b67b198e7bc46de4554f09a75a2271
Sikirou Bassirou <bassirousikirou59@gmail.com>
2025-01-21 14:46:45 +0100

    Mise à jour de mon image

--- src/ui/components/footer/footer.container.tsx
+++ src/ui/components/footer/footer.container.tsx
@@ -10,7 +10,7 @@ export default function FooterContainer() {
   return (
     <Container className="space-y-11 pb-11 bg-gray-100 dark:bg-gray-600">
       <div className="flex items-center justify-center gap-1">
-        <Typography variant="body3" theme="gray">
+        <Typography variant="body3" theme="gray" className="text-center">
           {`Copy @ ${currentYear} | Propulsed by `}{" "}
           <a href="#" className="underline">
             Eskay_dev
